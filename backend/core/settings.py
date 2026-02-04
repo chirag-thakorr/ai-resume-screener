3fc0731ac5a2131c72760d998551322527d9c03c
chirag-thakorr <chirag.thakor9828@gmail.com>
2026-02-04 08:14:22 +0530

    backend: apps registered

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'resumes',
+    'jobs',
+    'ai_engine',
+    'recruiter',
 ]
 
 MIDDLEWARE = [
